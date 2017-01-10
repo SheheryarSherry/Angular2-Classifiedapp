995b7a7d8f182fb34ed5359269e12840166e3fe4
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-10 23:05:15 +0500

    modification in classes

--- src/app/add.ts
+++ src/app/add.ts
@@ -15,3 +15,82 @@ export class Add {
         
     }
 }
+export class Phones extends Add{
+    screen:string;
+    ram:number;
+    processor:string;
+    
+    constructor(
+    model:string,
+    brand:string,
+    image:string,
+    devicecolor:string,
+    price:number,
+    product_details:string,
+    screen:string,
+    ram:number,
+    processor:string){
+super(model,brand,image,devicecolor,price,product_details);
+this.processor=processor;
+this.ram=ram;
+this.screen=screen;
+    }
+}
+export class tablets extends Add{
+    processor:string;
+    internalmemory:string;
+    screensize:number;
+    ram:number;
+    constructor(model:string,brand:string,image:string,devicecolor:string,price:number,product_details:string,
+    processor:string,
+    internalmemory:string,
+    screensize:number,
+    ram:number
+    ){
+        super(model,
+brand,
+image,
+devicecolor,
+price,
+product_details);
+    }
+}
+export class TV extends Add{
+    resolution:string;
+    quality:string;
+    screentype:string;
+    constructor(model:string,brand:string,image:string,devicecolor:string,price:number,product_details:string,
+    resolution:string,
+    quality:string,
+    screentype:string
+    ){
+        super(model,
+brand,
+image,
+devicecolor,
+price,
+product_details);
+    }
+    
+}
+
+export class Watch extends Add{
+    resolution:string;
+    storage:string;
+    ram:string;
+    screensize:string;
+    constructor(model:string,brand:string,image:string,devicecolor:string,price:number,product_details:string,
+    resolution:string,
+    storage:string,
+    screensize:string,
+    ram:string
+    ){
+        super(model,
+brand,
+image,
+devicecolor,
+price,
+product_details);
+    }
+    
+}
