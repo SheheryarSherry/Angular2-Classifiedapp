1c6129941f15498fccbf245ae58c59d0b538d199
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-10 19:18:29 +0500

    Class add modified

--- src/app/add.ts
+++ src/app/add.ts
@@ -1,2 +1,17 @@
 export class Add {
+    model:string;
+    brand:string;
+    image:string;
+    devicecolor:string;
+    addedincart=false;
+    product_details:string;
+    price:number;
+    constructor(model:string,brand:string,image:string,devicecolor:string,price:number,product_details:string){
+        this.model=model;
+        this.brand=brand;
+        this.image=image;
+        this.price=price;
+        this.product_details=product_details;
+        
+    }
 }
