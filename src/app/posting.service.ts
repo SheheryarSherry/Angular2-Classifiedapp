67e11138058990b46b8b251dfd80437bd0bb2e82
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-19 18:36:34 +0500

    initial Modification in services

--- src/app/posting.service.ts
+++ src/app/posting.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import {Add,Phones,Tablets,Tv,Watches} from "./Add"
 @Injectable()
+
 export class PostingService {
 
   constructor() { }
+  post:Add[]=[new Phones("LG G5","LG",)]
 
 }
